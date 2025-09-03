35a51905131b7e37d506f48b1df985def0aa5696
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-09-04 01:55:16 +0530

    minor fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -60,7 +60,7 @@ function Home() {
                   ))}
 
                 {/* View More Button */}
-                <div className="col-span-full flex justify-center mt-6">
+                <div className="col-span-full flex lg:justify-center mt-6">
                   <Link to="/events">
                     <button className="w-full sm:w-auto text-sm cursor-pointer transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
                       View More Events
@@ -111,7 +111,7 @@ function Home() {
                   ))}
 
                 {/* View More Button */}
-                <div className="col-span-full flex justify-center mt-6">
+                <div className="col-span-full  flex lg:justify-center mt-6">
                   <Link to="/events">
                     <button className="w-full sm:w-auto text-sm cursor-pointer transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
                       View More Events
--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,1 +1,1 @@
-export const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"
+export const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
